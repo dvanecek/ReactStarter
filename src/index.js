78f2e1c2648f0d066d4a2abeba57b67df1c7264c
Dan Vanecek <danv@seznam.cz>
2016-03-07 15:30:12 +0100

    Added dist folder for bundled js library. Added bootstrap css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './components/app';
+import App from './components/App.jsx';
 
 ReactDOM.render(
-    <App />, document.querySelector('.app')
+    <App />,
+    document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     output: {
         path: __dirname,
         publicPath: '/',
-        filename: 'dist/bundle.js'
+        filename: 'dist/js/bundle.js'
     },
     module: {
         loaders: [{
