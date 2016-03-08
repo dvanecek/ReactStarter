2e48136b1b9d284294f6f6d16e06ff5419a040a1
Dan Vanecek <danv@seznam.cz>
2016-03-08 22:37:36 +0100

    Rename dist to build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     output: {
         path: __dirname,
         publicPath: '/',
-        filename: 'dist/js/bundle.js'
+        filename: 'build/js/bundle.js'
     },
     module: {
         loaders: [{
